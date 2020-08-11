dc22ee44e678e806e27c730e54329b99b65951a4
FlorSanders <florsanders@gmail.com>
2020-08-11 10:36:45 +0200

    Sensor implementation and README with installation and usage instructions

---
+++ Sensor/Sensor_DHT/ProgramConfig.h
@@ -0,0 +1,23 @@
+#ifndef PROGRAM_CONFIG_H_
+#define PROGRAM_CONFIG_H_
+
+// WiFi Configuration
+#define WIFI_SSID "Your_SSID"
+#define WIFI_PASS "Your_Password"
+
+// DHT Sensor Configuration
+#define DHT_PIN D3
+#define DHT_TYPE DHT11
+
+// SenML Configuration
+#define SENSOR_NAME "Your_Sensor_Name"
+
+// NTP Client configuration
+#define NTP_SERVER "pool.ntp.org"
+
+// MQTT Client configuration
+#define MQTT_SERVER "Your_MQTT_Server"
+#define MQTT_TOPIC "Your_Topic"
+#define MSG_BUFFER_SIZE  256
+
+#endif
---
+++ Sensor/Sensor_DHT/Sensor_DHT.ino
@@ -0,0 +1,149 @@
+/***************************************
+ * Sensor_DHT
+ * Sensor: DHT11
+ * Data serialization: SenML
+ * Transport protocol: MQTT
+ * Author: Flor Sanders
+ * Version: 1.0
+*****************************************/
+
+// Including required libraries
+#include "DHT.h"
+#include <kpn_senml.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+#include "ProgramConfig.h"
+
+// Initializing DHT Object
+DHT dht(DHT_PIN, DHT_TYPE);
+
+// Initializing NTPClient Object
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_SERVER, 0);
+
+// Initializing SenML Objects
+SenMLPack doc(SENSOR_NAME);
+SenMLFloatRecord temperature(KPN_SENML_TEMPERATURE, SENML_UNIT_DEGREES_CELSIUS);
+SenMLFloatRecord humidity(KPN_SENML_HUMIDITY, SENML_UNIT_RELATIVE_HUMIDITY);
+
+// Initializing MQTT Objects
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[MSG_BUFFER_SIZE];
+
+// Reconnect to MQTT broker if connection was lost
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// Callback function for processing of incoming messages (if desired)
+void callback(char* topic, byte* payload, unsigned int length) {
+  return;
+}
+
+// Function to convert a MAC address to the corresponding EUI64 URI
+String MACtoEUI64(String MAC) {
+  for(int i = 1; i <= 5; i++) {
+    MAC.remove(2*i, 1);
+  }
+  String EUI64 = MAC.substring(0, 6) + "FFFE" + MAC.substring(6);
+  return EUI64;
+}
+
+// Setup function ran after every reset
+void setup() {
+  // Begin serial output
+  Serial.begin(115200);
+  Serial.println("Program started.");
+
+  // Connecting to wifi
+  Serial.println("Connecting to WiFi.");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(".");
+
+  // Initializing DHT Sensor
+  Serial.println("Starting DHT Sensor.");
+  dht.begin();
+
+  // Initializing the Time client
+  Serial.println("Starting NTP Client.");
+  timeClient.begin();
+
+  // Getting MAC and EUI64
+  String MAC = WiFi.macAddress();
+  String EUI64 = MACtoEUI64(MAC);
+  String URN = "urn:dev:mac:" + EUI64;
+
+  // Configuring the SenML Doc
+  Serial.println("Configuring SenML Doc.");
+  doc.setBaseName(URN.c_str());
+  doc.add(&temperature);
+  doc.add(&humidity);
+
+  // Configure the MQTT client
+  Serial.println("Configuring MQTT Client.");
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
+
+  Serial.println("Setup complete, starting loop.");
+}
+
+void loop() {
+  // Make sure the MQTT client has a connection to the broker
+  if(!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  // Wait a few seconds between measurements. (The DHT is a very slow sensor)
+  delay(2000);
+
+  // Reading humidity and temperature from the dht22
+  float humVal = dht.readHumidity();
+  float tempVal = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(tempVal) || isnan(humVal)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Updating the timeClient
+  timeClient.update();
+  double timeVal = timeClient.getEpochTime();
+
+  // Setting the new measured values in the document
+  temperature.set(tempVal, timeVal);
+  humidity.set(humVal, timeVal);
+  // Parsing the document to JSON and printing it to Serial output.
+  doc.toJson(msg, MSG_BUFFER_SIZE);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish(MQTT_TOPIC, msg);
+}

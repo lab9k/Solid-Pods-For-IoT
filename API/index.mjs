1b1cd179520a18e8b1a06adfef6e6f3245b0d0b9
FlorSanders <florsanders@gmail.com>
2020-08-27 14:37:21 +0200

    Created getFileList and getFile functions for static files

--- API/index.mjs
+++ API/index.mjs
@@ -1,7 +1,5 @@
 import fs from 'fs';
 
-
-
 function getFileList() {
     return new Promise((resolve, reject) => {
         fs.readdir('static', (err, files) => {
@@ -10,10 +8,26 @@ function getFileList() {
             } else {
                 resolve(files);
             }
-        })
-    })
+        });
+    });
+}
+
+function getFile(file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('static/' + file, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data.toString());
+            }
+        });
+    });
 }
 
 getFileList()
     .then(files => console.log(files))
+    .catch(err => console.error(err));
+
+getFile('saref1.ttl')
+    .then(content => console.log(content))
     .catch(err => console.error(err));

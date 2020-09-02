6e212fac4aaa790e62a6f643905a58382f143b9c
FlorSanders <florsanders@gmail.com>
2020-09-02 14:28:29 +0200

    Switched over to Digipolis Lab9K Logo's

--- Dashboard/src/components/NavBar/nav-bar.component.js
+++ Dashboard/src/components/NavBar/nav-bar.component.js
@@ -58,7 +58,7 @@ const NavBar = (props: Props) => {
       <section className="header-wrap">
         <div className="logo-block">
           <Link to="/welcome">
-            <img src="/img/inrupt.svg" alt="inrupt" />
+            <img src="/img/lab9ksquare.png" alt="inrupt" />
           </Link>
         </div>
 

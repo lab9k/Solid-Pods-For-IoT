5f5851c4129bfea5498778f24668ef63c55f99aa
FlorSanders <florsanders@gmail.com>
2020-09-02 12:01:44 +0200

    Code cleanup

--- Dashboard/src/containers/index.js
+++ Dashboard/src/containers/index.js
@@ -2,10 +2,7 @@ import Login from './Login';
 import Register, { RegistrationSuccess } from './Register';
 import PageNotFound from './PageNotFound';
 import Welcome from './Welcome';
-import Profile from './Profile';
-import TextEditor from './TextEditor';
 import IoTDashboard from './IoTDashboard';
-import { FormModelConverter, FormModelRenderer } from './FormModel';
 
 export {
   Login,
@@ -14,8 +11,4 @@ export {
   PageNotFound,
   IoTDashboard,
   Welcome,
-  Profile,
-  FormModelConverter,
-  FormModelRenderer,
-  TextEditor
 };

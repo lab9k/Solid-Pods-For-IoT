18e71a0918685799378f180b0a309235c5ad1976
FlorSanders <florsanders@gmail.com>
2020-09-02 11:13:36 +0200

    Acces control now *actually* works (without removing our own access) and messages to the Solid API get now sent

--- Dashboard/src/containers/IoTDashboard/iot-dashboard.component.js
+++ Dashboard/src/containers/IoTDashboard/iot-dashboard.component.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import aclClient from 'ownacl';
 import { errorToaster, successToaster } from '@utils';
 import { AccessControlList } from '@inrupt/solid-react-components';
 import { retrieveStore, getFiles, getSensor, getMeasurements, getData } from './utils';
@@ -11,6 +13,12 @@ import {
 
 const ACCESSLISTWEBID = 'https://flordigipolis.solidweb.org/profile/card#me';
 const IOTFOLDER = 'private/iot';
+const APIURL = 'http://solid.pool42.io:8030/v1/';
+
+const api = axios.create({
+    baseURL: APIURL,
+    timeout: 3000
+});
 
 export class IoTDashboard extends React.Component {
     // Program state
@@ -24,15 +32,17 @@ export class IoTDashboard extends React.Component {
         data: []
     }
 
+    // Immediately after mounting, the list of the files in the /private/iot subfolder is read
     componentDidMount() {
         getFiles(IOTFOLDER)
-            .then((files) => this.setState({files}))
-            .catch((err) => errorToaster(err));
+            .then((files) => this.setState({ files }))
+            .catch((err) => errorToaster(String(err)));
     }
 
+    // Upon receival of the file name, we need to check share access and get the data out of it
     onReceiveFile = (file) => {
         this.setState({ file, data: [], shared: undefined }, () => {
-            if(this.state.file !== 'None'){
+            if (this.state.file !== 'None') {
                 retrieveStore(this.state.file)
                     .then(({ store, fetcher, webId }) => {
                         // Save the store and fetcher into the program state
@@ -41,34 +51,61 @@ export class IoTDashboard extends React.Component {
                             this.onReceiveStore();
                         });
                     })
-                    .catch(err => errorToaster(err));
+                    .catch(err => errorToaster(String(err)));
             }
         })
     }
 
+    // When the data is in the store, we need to get the data out of it
     onReceiveStore = () => {
         // Getting sensor data
-        var {sensor, type} = getSensor(this.state.store);
+        var { sensor, type } = getSensor(this.state.store);
         if (type === 'sosa' || type === 'saref') {
             var measurements = getMeasurements(sensor, type, this.state.store);
             var data = getData(measurements, type, this.state.store);
-            this.setState({data});
+            this.setState({ data });
         } else {
             errorToaster('No SSN/SOSA or SAREF device found in this file.');
         }
     }
 
+    // Checks if our file already has an acl file, if not creates one, in the end we know whether the access list webId already has access. Admittedly it's a mess
     updateShareStatus = () => {
-        // Getting ACL status
+        var aclurl = `${this.state.file}.acl`;
+        // Check if an ACL file already exists
         const ACLFile = new AccessControlList(this.state.webId, this.state.file);
-        ACLFile.getPermissions().then(permissions => {
-            console.log(permissions);
-            var filtered_permissions = permissions.filter((permission) => {
-                console.log(permissions);
-                // Check if the WebId where the access list is stored is already listed under read permissions
-                return permission.agents.includes(ACCESSLISTWEBID) && permission.modes.includes("Read");
-            });
-            this.setState({shared: !!filtered_permissions.length});
+        ACLFile.getACLFile().then(result => {
+            // Checking if our file already has its own acl file
+            if (result.url === aclurl) {
+                // ACL File is already present
+                console.log('ACL File Found');
+                // Check file permissions
+                const acl = new aclClient(aclurl);
+                acl.readAccessControl().then(permissions => {
+                    var filtered_permissions = permissions.filter((permission) => {
+                        return permission.name === ACCESSLISTWEBID;
+                    });
+                    this.setState({ shared: !!filtered_permissions.length });
+                }).catch(err => errorToaster(err));
+            } else {
+                // ACL File is not yet present
+                console.log('ACL File not yet found');
+                // Get old permissions
+                ACLFile.getPermissions().then((old_permissions) => {
+                    // Create ACL file with old permissions
+                    ACLFile.createACL(old_permissions).then(() => {
+                        // Get new permissions
+                        const acl = new aclClient(aclurl);
+                        acl.readAccessControl().then(permissions => {
+                            console.log(permissions)
+                            var filtered_permissions = permissions.filter((permission) => {
+                                return permission.name === ACCESSLISTWEBID;
+                            });
+                            this.setState({ shared: !!filtered_permissions.length });
+                        }).catch(err => errorToaster(err));
+                    }).catch(err => errorToaster(err));
+                }).catch(err => errorToaster(err));
+            }
         }).catch(err => errorToaster(err));
     }
 
@@ -76,57 +113,73 @@ export class IoTDashboard extends React.Component {
     giveReadPermissions = async (url) => {
         try {
             // Fix the access control list
-            const ACLFile = new AccessControlList(this.state.webId, this.state.file);
-            // Don't forget to repeat the existing permissions to make sure you don't lock yourself out.
-            var existingpermissions = await ACLFile.getPermissions();
-            const permissions = [
-                ...existingpermissions,
-                {   
-                    agents: [url],
-                    modes: [AccessControlList.MODES.READ]
-                }
-            ];
-            ACLFile.assignPermissions(permissions).then(() => {
+            const acl = new aclClient(`${this.state.file}.acl`);
+            const toAdd = {
+                name: url,
+                access: ['Read']
+            };
+            acl.addAgent(toAdd).then(() => {
                 this.updateShareStatus();
-                if (url === ACCESSLISTWEBID) {
-                    // Also send a put request to our API to let it know access was granted
-                }
                 successToaster(`Read access granted to ${url}.`);
+                if (url === ACCESSLISTWEBID) {
+                    const urlcomponents = this.state.file.split('/');
+                    const filename = urlcomponents[urlcomponents.length - 1];
+                    // Also send a put request to our API to let it know access was granted
+                    api.put(`/solidfiles/${filename}`, {
+                        title: filename,
+                        address: this.state.file
+                    }).then((res) => {
+                        console.log(res);
+                        successToaster('File added in Digipolis API.');
+                    }).catch(err => errorToaster(String(err)));
+                }
             });
-            
+
         } catch (err) {
             errorToaster(`Error granting access to ${url}: ${err}`);
         }
     }
 
-    takeReadPermissions = async(url) => {
+    takeReadPermissions = async (url) => {
         try {
-            const permissions = [
-                {
-                    agents: [url],
-                    modes: [AccessControlList.MODES.READ]
-                }
-            ];
-            const ACLFile = new AccessControlList(this.state.webId, this.state.file);
-            ACLFile.removePermissions(permissions).then(() => {
+            // Fix the access control list
+            const acl = new aclClient(`${this.state.file}.acl`);
+            const toRemove = {
+                name: url,
+                access: ['Read']
+            };
+            acl.deleteAgent(toRemove).then(() => {
                 this.updateShareStatus();
-                if (url === ACCESSLISTWEBID) {
-                    // Also send a put request to our API to let it know access was granted
-                }
                 successToaster(`Read access removed from ${url}.`);
+                if (url === ACCESSLISTWEBID) {
+                    const urlcomponents = this.state.file.split('/');
+                    const filename = urlcomponents[urlcomponents.length - 1];
+                    // Also send a delete request to our API to let it know access was granted
+                    api({
+                        method: 'DELETE',
+                        url: `/solidfiles/${filename}`,
+                        data: {
+                            title: filename,
+                            address: this.state.file
+                        }
+                    }).then((res) => {
+                        console.log(res);
+                        successToaster('File removed in Digipolis API');
+                    }).catch(err => errorToaster(String(err)));
+                }
             });
         } catch (err) {
-            errorToaster(err);
+            errorToaster(String(err));
         }
-    } 
+    }
 
     // onSubmit function for the Digipolis share button
-    onShareDigipolis = async (url) => {
+    onShareDigipolis = () => {
         if (this.state.shared) {
-            console.log('We need to unshare the file');
+            // The file was shared, now we need to take away the permissions
             this.takeReadPermissions(ACCESSLISTWEBID);
         } else {
-            console.log('We need to share the file');
+            // The file wasn't shared, now we need to add permissions
             this.giveReadPermissions(ACCESSLISTWEBID);
         }
     }
@@ -144,7 +197,7 @@ export class IoTDashboard extends React.Component {
                     <Selectinput onSubmit={this.onReceiveFile} options={this.state.files} label="Select a file" option={this.state.file}></Selectinput>
                     {(this.state.file !== 'None') ? <Textinput onSubmit={this.giveReadPermissions} default={ACCESSLISTWEBID} title='Share this file' label='share'></Textinput> : <></>}
                     {(this.state.file !== 'None' && this.state.shared !== undefined) ? <Shareinput onSubmit={this.onShareDigipolis} title='Share this file' shared={this.state.shared}></Shareinput> : <></>}
-                    <Visualize data = {this.state.data} object = {this.state.file} type = {this.state.type}></Visualize>
+                    <Visualize data={this.state.data} object={this.state.file} type={this.state.type}></Visualize>
                 </IoTDashboardContainer>
             </IoTDashboardWrapper>
         )
